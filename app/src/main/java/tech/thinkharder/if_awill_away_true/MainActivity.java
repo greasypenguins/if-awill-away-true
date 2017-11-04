5599463d5beade91a86124a72ef354b404e651c1
greasypenguins <mrwestonharder@gmail.com>
2017-11-04 01:09:44 -0500

    Added blank app
    Added a blank Android Studio app targeted for 5.0 (API 21). This time it should work.

---
+++ app/src/main/java/tech/thinkharder/if_awill_away_true/MainActivity.java
@@ -0,0 +1,13 @@
+package tech.thinkharder.if_awill_away_true;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
